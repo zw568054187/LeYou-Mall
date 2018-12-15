04be0ae8470068081b17187aecfd568170d42666
zhangwei <568054187@qq.com>
2018-12-15 20:24:26 +0800

    修改了基本网关配置

--- ly-api-gateway/src/main/java/com/leyou/LyApiGateway.java
+++ ly-api-gateway/src/main/java/com/leyou/LyApiGateway.java
@@ -1,16 +1,15 @@
 package com.leyou;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author zhangwei
  * @date 2018/12/13 21:04
  */
-@SpringBootApplication
-@EnableEurekaClient
+
+@SpringCloudApplication
 @EnableZuulProxy
 public class LyApiGateway {
     public static void main(String[] args) {
